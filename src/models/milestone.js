a650eb0bb2959017a4b10b91589c6e38769ac17c
Uzukwu, John Paul <jchuks69@gmail.com>
2020-08-17 17:44:58 +0100

    modified milestone model by adding strict option object which enables schemaless document

--- src/models/milestone.js
+++ src/models/milestone.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-const mileStoneSchema = new mongoose.Schema({
-  projectName: { type: String, required: true },
-  stageName: { type: String, required: true },
-  mileStones: { type: mongoose.Schema.Types.Mixed, required: true },
-});
+const mileStoneSchema = new mongoose.Schema(
+  {
+    projectName: { type: String, required: true },
+    stageName: { type: String, required: true },
+    mileStones: { type: mongoose.Schema.Types.Mixed, required: true },
+  },
+  { strict: false }
+);
 
 export default mongoose.model("Milestone", mileStoneSchema);
