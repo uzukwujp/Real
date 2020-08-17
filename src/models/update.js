bbf30063bdda7ccf02ad8c33e23228ee1684ce56
Uzukwu, John Paul <jchuks69@gmail.com>
2020-08-17 16:03:06 +0100

    modified update model by adding update schedule field

--- src/models/update.js
+++ src/models/update.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
 
-const updateSchema = new mongoose.Schema(
-  {
-    projectName: { type: String, required: true },
-    message: { type: String, required: true },
-    lastUpdate: { type: Date },
-    nextUpdate: { type: Date },
-  },
-  { timestamps: true }
-);
+const updateSchema = new mongoose.Schema({
+  projectName: { type: String, required: true },
+  message: { type: String, required: true },
+  updateSchedule: { type: mongoose.Types.ObjectId },
+});
 
 export default mongoose.model("Update", updateSchema);
